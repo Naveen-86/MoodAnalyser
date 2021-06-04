8890555b3b03d612cd527791f717f64a9fbf9721
Naveen <naveentruly86@gmail.com>
2021-06-04 18:46:28 +0530

    Added: Ability to Analyse Happy or Sad Mood

--- src/main/java/in/moodproblem/MoodAnalyser.java
+++ src/main/java/in/moodproblem/MoodAnalyser.java
@@ -1,7 +1,20 @@
 package in.moodproblem;
 
 public class MoodAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Mood Analyser Problem");
+
+    String message;
+    public MoodAnalyser() {
+    }
+
+    MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyserMood() {
+        if (message.contains(("sad"))) {
+            return "SAD";
+        }else {
+            return "HAPPY";
+        }
     }
 }
--- src/test/java/in/moodproblem/MoodAnalyserTest.java
+++ src/test/java/in/moodproblem/MoodAnalyserTest.java
@@ -1,10 +1,22 @@
 package in.moodproblem;
 
-import org.junit.jupiter.api.Test;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 
 public class MoodAnalyserTest {
     @Test
     public void testMoodAnalysis() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String mood = moodAnalyser.analyserMood();
+        Assert.assertThat(mood,CoreMatchers.is("SAD"));
+    }
+
+    @Test
+    public void testMoodAnalysis1() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any Mood");
+        String mood = moodAnalyser.analyserMood();
+        Assert.assertThat(mood,CoreMatchers.is("HAPPY"));
     }
 }
