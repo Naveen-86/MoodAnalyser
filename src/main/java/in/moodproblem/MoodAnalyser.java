0ddfa2790f4237fe85c63497aefedf28f2a1c5a1
Naveen <naveentruly86@gmail.com>
2021-06-04 16:43:56 +0530

    Added: Welcome Message

---
+++ src/main/java/in/moodproblem/MoodAnalyser.java
@@ -0,0 +1,7 @@
+package in.moodproblem;
+
+public class MoodAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyser Problem");
+    }
+}
---
+++ src/test/java/in/moodproblem/MoodAnalyserTest.java
@@ -0,0 +1,10 @@
+package in.moodproblem;
+
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyserTest {
+    @Test
+    public void testMoodAnalysis() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+    }
+}
